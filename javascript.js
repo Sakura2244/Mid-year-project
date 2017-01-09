155985db4896901a460a29a57dcbe752c16e9835
Jennifer <jenniferzheng23@gmail.com>
2017-01-09 22:11:33 +0000

    add html,js,and css

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// JavaScript File
+$("#home-button").click(function(){
+    $(".all-content").hide();
+    $("#home-content").show();
+});
+$("#clubs-button").click(function(){
+    $(".all-content").hide();
+    $("#clubs-content").show();
+});
+$("#teachers-button").click(function(){
+    $(".all-content").hide();
+    $("#teachers-content").show();
+});
